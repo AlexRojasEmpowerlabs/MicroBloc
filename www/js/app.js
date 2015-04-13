98d0fe29c34b42d8688cab83d5252f6a142a8a7a
Alex Rojas <jose.rojas@empowerlabs.com>
2015-04-13 01:04:42 -0500

    thumbs

--- www/js/app.js
+++ www/js/app.js
@@ -54,7 +54,7 @@ video=videos[0];
         }*/
        video.src = selectedBloc.VideoResource;
 	$scope.video=function(item){
-    	$scope.ons.notification.confirm({messageHTML: '<ons-row><ons-col width="95px"><img src="../img/icon.png" class="thumbnail"></ons-col><ons-col><div class="name"> '+item.Title+' </div><div class="location"><i class="fa fa-map-marker"></i> Paris, France</div><div class="desc"> '+item.VideoResource+'</div></ons-col> <ons-col width="40px"></ons-col> </ons-row>',title: "¿Compartir este video?"});
+    	$scope.ons.notification.confirm({messageHTML: '<ons-row><ons-col width="95px"><img src="img/YouTube.png" class="thumbnail"></ons-col><ons-col><div class="name"> '+item.Title+' </div><div class="location"><i class="fa fa-map-marker"></i> Paris, France</div><div class="desc"> '+item.VideoResource+'</div></ons-col> <ons-col width="40px"></ons-col> </ons-row>',title: "¿Compartir este video?"});
     	//$scope.ons.navigator.pushPage('video.html', {title : "1"});
     	}; 
     $scope.audio=function(item){
@@ -68,7 +68,7 @@ video=videos[0];
     	$scope.ons.notification.alert({messageHTML:'<div style="width: 100%; height: 360px;background-color:White; overflow: hidden;"><iframe src="//www.slideshare.net/slideshow/embed_code/46732684?rel=0" style="width:80%" height="407" frameborder="0" marginwidth="0" marginheight="0" scrolling="no"></iframe></div>',title:'Presentación'});
     	};
     $scope.shareWeb=function(item){
-    	$scope.ons.notification.confirm({messageHTML: '<ons-row><ons-col width="95px"><img src="'+item.PresentationResource+'" class="thumbnail"></ons-col><ons-col><div class="name"> '+item.Title+' </div><div class="location"><i class="fa fa-map-marker"></i> Paris, France</div><div class="desc"> '+item.ExternalLink+'</div></ons-col> <ons-col width="40px"></ons-col> </ons-row>',title: "¿Compartir Bloc en la web?"});
+    	$scope.ons.notification.confirm({messageHTML: '<ons-row><ons-col width="95px"><img src="'+item.Thumbnail+'" class="thumbnail"></ons-col><ons-col><div class="name"> '+item.Title+' </div><div class="location"><i class="fa fa-map-marker"></i> Paris, France</div><div class="desc"> '+item.ExternalLink+'</div></ons-col> <ons-col width="40px"></ons-col> </ons-row>',title: "¿Compartir Bloc en la web?"});
     };
   });
   
